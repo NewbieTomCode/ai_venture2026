9113dead4e00a88429d286dd2fef3500388a766d
Thomas <thomaswy2000@gmail.com>
2026-02-07 14:48:31 +0000

    vid gen completed

--- backend/app/services/video_generator.py
+++ backend/app/services/video_generator.py
@@ -4,38 +4,40 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+def generate_veo_video(prompt_text):
+    load_dotenv(find_dotenv())
+    project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
+    bucket_path = os.getenv("BUCKET_PATH")
 
-# The client automatically picks up GOOGLE_CLOUD_PROJECT,
-# GOOGLE_CLOUD_LOCATION, and GOOGLE_APPLICATION_CREDENTIALS
-client = genai.Client(
-    vertexai=True,
-    project=os.getenv("GOOGLE_CLOUD_PROJECT"),
-    location=os.getenv("GOOGLE_CLOUD_LOCATION", "us-central1")
-)
+    client = genai.Client(
+        vertexai=True, 
+        project=project_id, # <-- Put your Project ID here
+        location="us-central1"
+    )
 
-prompt = """A close up of two people staring at a cryptic drawing on a wall, torchlight flickering.
-A man murmurs, 'This must be it. That's the secret code.' The woman looks at him and whispering excitedly, 'What did you find?'"""
+    # Create a unique path for every run
+    timestamp = int(time.time())
+    full_uri = f"{bucket_path}/veo_video_{timestamp}.mp4"
+    print(timestamp)
 
-operation = client.models.generate_videos(
-    model="veo-3.1-generate-001",
-    prompt=prompt,
-    config=types.GenerateVideosConfig(
-      aspect_ratio="9:16",
-      resolution="720p",
-    ),
-)
+    operation = client.models.generate_videos(
+        model="veo-3.1-generate-001",
+        prompt=prompt_text,
+        config=types.GenerateVideosConfig(
+        aspect_ratio="9:16",
+        resolution="720p",
+        output_gcs_uri=full_uri
+        ),
+    )
 
-# Poll the operation status until the video is ready.
-while not operation.done:
-    print("Waiting for video generation to complete...")
-    time.sleep(10)
-    operation = client.operations.get(operation)
+    print(f"Generation started... Target: {full_uri}")
 
-# Use this refined download block
-generated_video = operation.result.generated_videos[0]
-print(f"Video ready at: {generated_video.video.uri}")
+    while not operation.done:
+        time.sleep(10)
+        operation = client.operations.get(operation)
 
-# Download the content
-client.files.download(file=generated_video.video, path="/Users/test/Downloads/my_video.mp4")
-print("Generated video saved to dialogue_example.mp4")
+    print("Video generation complete! Check your bucket now.")
+
+PROMPT = """ 3 pigs eating corns """
+
+generate_veo_video(PROMPT)
